5239a6171b1e731f3a3938603f214a3183afdcbf
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-03-25 18:45:08 -0300

    :sparkles:

---
+++ data.test.js
@@ -0,0 +1,104 @@
+const aa = {
+  html_url: "{=UrlRegistro}",
+  isCorporate: "{!isCorporate}",
+  tradeName: "{!tradeName}",
+  rclastcart: "{!rclastcart}",
+  rclastcartvalue: "{!rclastcartvalue}",
+  rclastsession: "{!rclastsession}",
+  rclastsessiondate: "{!rclastsessiondate}",
+  homePhone: "{!homePhone}",
+  phone: "{!phone}",
+  brandPurchasedTag: "{!brandPurchasedTag}",
+  brandVisitedTag: "{!brandVisitedTag}",
+  categoryPurchasedTag: "{!categoryPurchasedTag}",
+  categoryVisitedTag: "{!categoryVisitedTag}",
+  departmentVisitedTag: "{!departmentVisitedTag}",
+  productPurchasedTag: "{!productPurchasedTag}",
+  productVisitedTag: "{!productVisitedTag}",
+  stateRegistration: "{!stateRegistration}",
+  email: "{!email}",
+  userId: "{!userId}",
+  firstName: "{!firstName}",
+  lastName: "{!lastName}",
+  document: "{!document}",
+  isNewsletterOptIn: "{!isNewsletterOptIn}",
+  localeDefault: "{!localeDefault}",
+  attach: "{!attach}",
+  approved: "{!approved}",
+  birthDate: "{!birthDate}",
+  businessPhone: "{!businessPhone}",
+  carttag: "{!carttag}",
+  checkouttag: "{!checkouttag}",
+  corporateDocument: "{!corporateDocument}",
+  corporateName: "{!corporateName}",
+  documentType: "{!documentType}",
+  gender: "{!gender}",
+  visitedProductWithStockOutSkusTag: "{!visitedProductWithStockOutSkusTag}",
+  customerClass: "{!customerClass}",
+  priceTables: "{!priceTables}",
+  profilePicture: "{!profilePicture}",
+  birthDateMonth: "{!birthDateMonth}",
+  id: "{!id}",
+  accountId: "{!accountId}",
+  accountName: "{!accountName}",
+  dataEntityId: "{!dataEntityId}",
+  createdBy: "{!createdBy}",
+  createdIn: "{!createdIn}",
+  updatedBy: "{!updatedBy}",
+  updatedIn: "{!updatedIn}",
+  lastInteractionBy: "{!lastInteractionBy}",
+  lastInteractionIn: "{!lastInteractionIn}",
+  followers: "{!followers}",
+  tags: "{!tags}",
+  auto_filter: "{!auto_filter}",
+};
+
+const bb = {
+  ProfileKey: "NjYzODYw0",
+  Contact: {
+    Email: "{!email}",
+    CustomFields: [
+      {
+        Key: "nombre",
+        Value: "{!firstName}",
+      },
+      {
+        Key: "apellido",
+        Value: "{!lastName}",
+      },
+      {
+        Key: "url_carrito",
+        Value: "{!rclastcart}",
+      },
+      {
+        Key: "fecha_abandono",
+        Value: "{!rclastsessiondate}",
+      },
+    ],
+  },
+};
+
+const cc = {
+  ProfileKey: "NjYzODYw0",
+  Contact: {
+    Email: "rubene250+1@outlook.com",
+    CustomFields: [
+      {
+        Key: "nombre",
+        Value: "Ruben",
+      },
+      {
+        Key: "apellido",
+        Value: "Mavarez",
+      },
+      {
+        Key: "url_carrito",
+        Value: "google.com",
+      },
+      {
+        Key: "fecha_abandono",
+        Value: "",
+      },
+    ],
+  },
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const moment = require("moment");
+const express = require("express");
+const app = express();
+
+app.listen(3001, () => {
+  console.log("Servidor levantado en el puerto 3001");
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("lenny was here");
+});
+
+app.post("/algo", (req, res) => {
+  let data = req.body;
+
+  data.Contact.CustomFields = data.Contact.CustomFields.map(
+    ({ Key, Value }) => {
+      if (Key === "fecha_abandono") Value = moment(Value).format("DD-MM-YYYY");
+
+      return {
+        Key,
+        Value,
+      };
+    }
+  );
+
+  res.send(data);
+});
